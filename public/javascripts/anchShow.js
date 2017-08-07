89f7cfd64053099ea0701d67325508db71c58c15
zhuth <zthbruce@bupt.edu.cn>
2017-08-07 20:21:30 +0800

    提交

--- public/javascripts/anchShow.js
+++ public/javascripts/anchShow.js
@@ -1,7 +1,6 @@
-// var anchInfoList = [];
+var anchInfoList = [];
 
 function getAllAnch() {
-    var anchInfoList = [];
     $.ajax({
         url: "/anch/getAnchShowInfo",
         dataType: 'json',
@@ -38,7 +37,7 @@ function getAllAnch() {
                         })
                     }
                 }
-                anchLayer(anchInfoList)
+                anchLayer(4)
             }
         },
         error: function (data, status, e) {
@@ -46,54 +45,106 @@ function getAllAnch() {
         }
     })
 }
+//
+// function getLonLatList() {
+//
+//
+// }
 
 /**
  *  根据锚地表显示相应的区域
  */
-function anchLayer(anchInfoList){
-    console.log("加载锚地区域");
-    console.log(anchInfoList);
-    anch.getSource().clear();
-    var features = [];
-    for(var i = 0; i < anchInfoList.length; i++){
-        var anch_info = anchInfoList[i];
-        var name = anch_info.name;
-        var anchorageKey = anch_info.anchorageKey;
-        var lon = anch_info.lon;
-        var lat = anch_info.lat;
-        console.log(anchorageKey);
-        console.log(name);
-        var anch_style =  new ol.style.Style({
-            stroke: new ol.style.Stroke({
-                color: 'blue',
-                lineDash: [1, 2, 3, 4, 5, 6],
-                width: 3
-            }),
-            fill: new ol.style.Fill({
-                color: 'rgba(0, 0, 255, 0.1)'
-            }),
-            text : new ol.style.Text({
-                // font: '10px sans-serif' 默认这个字体，可以修改成其他的，格式和css的字体设置一样
-                font: "15px",
-                text: name !== "" ? name : "未命名锚地",
-                fill: new ol.style.Fill({
-                    color: 'black'
+function anchLayer(level){
+    if (level >= 10) {
+        console.log("加载锚地详细区域");
+        // console.log(anchInfoList);
+        anch.getSource().clear();
+        // var features = [];
+        for (var i = 0; i < anchInfoList.length; i++) {
+            var anch_info = anchInfoList[i];
+            var name = anch_info.name;
+            var anchorageKey = anch_info.anchorageKey;
+            var lon = anch_info.lon;
+            var lat = anch_info.lat;
+            console.log(anchorageKey);
+            console.log(name);
+            var anch_style = new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'blue',
+                    lineDash: [1, 2, 3, 4, 5, 6],
+                    width: 3
                 }),
-                textAlign:"center"
-            })
-        });
-        // 获取特性
-        console.log(anch_info.lonLatList);
-        var feature = new ol.Feature({
-            type: 0,
-            anchKey: anchorageKey,
-            lon: lon,
-            lat: lat,
-            geometry: new ol.geom.Polygon([anch_info.lonLatList])
-        });
-        feature.setStyle(anch_style);
-        // features.push(feature);
-        anch.getSource().addFeature(feature);
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                }),
+                text: new ol.style.Text({
+                    // font: '10px sans-serif' 默认这个字体，可以修改成其他的，格式和css的字体设置一样
+                    font: "15px",
+                    text: name !== "" ? name : "未命名锚地",
+                    fill: new ol.style.Fill({
+                        color: 'black'
+                    }),
+                    textAlign: "center"
+                })
+            });
+            // 获取特性
+            console.log(anch_info.lonLatList);
+            var feature = new ol.Feature({
+                type: 0,
+                anchKey: anchorageKey,
+                lon: lon,
+                lat: lat,
+                geometry: new ol.geom.Polygon([anch_info.lonLatList])
+            });
+            feature.setStyle(anch_style);
+            // features.push(feature);
+            anch.getSource().addFeature(feature);
+        }
+        // anch.getSource().addFeatures(features);
     }
-    // anch.getSource().addFeatures(features);
+    else{
+        console.log("加载中心位置信息");
+        anch.getSource().clear();
+        // var features = [];
+        for (var i = 0; i < anchInfoList.length; i++) {
+            var anch_info = anchInfoList[i];
+            // var name = anch_info.name;
+            var anchorageKey = anch_info.anchorageKey;
+            var lon = anch_info.lon;
+            var lat = anch_info.lat;
+            // console.log(anchorageKey);
+            // console.log(name);
+            // var anch_style = new ol.style.Style({
+            //     stroke: new ol.style.Stroke({
+            //         color: 'blue',
+            //         lineDash: [1, 2, 3, 4, 5, 6],
+            //         width: 3
+            //     }),
+            //     fill: new ol.style.Fill({
+            //         color: 'rgba(0, 0, 255, 0.1)'
+            //     }),
+            //     text: new ol.style.Text({
+            //         // font: '10px sans-serif' 默认这个字体，可以修改成其他的，格式和css的字体设置一样
+            //         font: "15px",
+            //         text: name !== "" ? name : "未命名锚地",
+            //         fill: new ol.style.Fill({
+            //             color: 'black'
+            //         }),
+            //         textAlign: "center"
+            //     })
+            // });
+            // 获取特性
+            // console.log(anch_info.lonLatList);
+            var feature = new ol.Feature({
+                type: 0,
+                anchKey: anchorageKey,
+                lon: lon,
+                lat: lat,
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat]))
+                // geometry: new ol.geom.Polygon([anch_info.lonLatList])
+            });
+            feature.setStyle(exist_anch_style);
+            anch.getSource().addFeature(feature);
+        }
+    }
 }
